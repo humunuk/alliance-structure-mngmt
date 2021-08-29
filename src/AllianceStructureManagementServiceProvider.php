d522ba3608535c414ef8824dfc47920745b94752
Siim Kallari <siim.kallari@gmail.com>
2021-08-29 12:03:01 +0300

    Initial service provider

---
+++ src/AllianceStructureManagementServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Humunuk\Seat\AllianceStructureManagement;
+
+
+use Seat\Services\AbstractSeatPlugin;
+
+class AllianceStructureManagementServiceProvider extends AbstractSeatPlugin
+{
+
+    public function getName(): string
+    {
+        return "Alliance Structure Management";
+    }
+
+    public function getPackageRepositoryUrl(): string
+    {
+        return "https://github.com/humunuk/alliance-structure-mngmt";
+    }
+
+    public function getPackagistPackageName(): string
+    {
+        return "seat-alliance-structure-management";
+    }
+
+    public function getPackagistVendorName(): string
+    {
+        return "humunuk";
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/Config/package.alliance.menu.php', 'package.alliance.menu');
+    }
+}
