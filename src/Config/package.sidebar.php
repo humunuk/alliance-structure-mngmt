4d6ada319682350e8141857ad878b6043abfd9a4
Siim Kallari <siim.kallari@gmail.com>
2021-08-29 12:32:49 +0300

    more testing

--- src/Config/package.sidebar.php
+++ src/Config/package.sidebar.php
@@ -3,6 +3,8 @@
 return [
   'testing_sidebar' => [
       'permission' => 'global.superuser',
-      'name' => 'testing_sidebar'
+      'name' => 'testing_sidebar',
+      'route_segment' => 'api-admin',
+      'route'         => 'api-admin.list',
   ]
 ];
