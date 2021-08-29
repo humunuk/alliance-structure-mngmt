af1dbe99d2351fec0d7bc75fca9921445fad3b10
Siim Kallari <siim.kallari@gmail.com>
2021-08-29 12:34:36 +0300

    more testing

--- src/Config/package.sidebar.php
+++ src/Config/package.sidebar.php
@@ -6,5 +6,6 @@ return [
       'name' => 'testing_sidebar',
       'route_segment' => 'api-admin',
       'route'         => 'api-admin.list',
+      'icon'          => 'fas fa-exchange-alt',
   ]
 ];
